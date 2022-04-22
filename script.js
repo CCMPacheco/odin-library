8ac1355c31354486ee252ce291cff524afd940d8
CCMPacheco <pachecoccmp@gmail.com>
2022-04-22 11:21:56 -0300

    Add modal functionality

--- script.js
+++ script.js
@@ -8,3 +8,32 @@ function Book(title, author, pages, readStatus) {
 }
 
 function addBookToLibrary() {}
+
+function openModal() {
+  modal.classList.add("active");
+  overlay.classList.add("active");
+}
+
+function closeModal() {
+  modal.classList.remove("active");
+  overlay.classList.remove("active");
+}
+
+const addBtn = document.querySelector("[data-add-btn]");
+const closeBtn = document.querySelector("[data-close-btn]");
+const readBtn = document.querySelector("[data-read-btn]");
+const removeBtn = document.querySelector("[data-remove-btn]");
+const overlay = document.querySelector(".overlay");
+const modal = document.getElementById("modal");
+
+addBtn.addEventListener("click", () => {
+  openModal();
+});
+
+closeBtn.addEventListener("click", () => {
+  closeModal();
+});
+
+overlay.addEventListener("click", () => {
+  closeModal();
+});
